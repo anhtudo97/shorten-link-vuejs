068dddd483655cfbeaa35c1e3840dcee92f9b920
Do Anh Tu <anhtudo97@gmail.com>
2020-08-24 17:04:18 +0700

    feat: home

---
+++ components/shares/Header.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="mx-auto py-3">
+    <div class="display-small-hidden mx-md-6 mx-lg-8 mx-xl-10 mx-0">
+      <div class="header d-flex absolute align-center">
+        <div class="header__logo">
+          <img src="~/assets/logo.png" />
+        </div>
+        <v-spacer></v-spacer>
+        <ul>
+          <li v-for="item in menu" :key="item.id">
+            <nuxt-link :to="item.route" exact-active-class="nuxt-link-active">
+              <div class="header__menu">
+                <div class="menu-text">{{item.name}}</div>
+              </div>
+            </nuxt-link>
+          </li>
+        </ul>
+        <v-spacer></v-spacer>
+        <div class="header__login mr-5">
+          <div>Login</div>
+        </div>
+        <div class="header__signup py-3 px-6">
+          <div class="signup-text">Sign up</div>
+        </div>
+      </div>
+    </div>
+    <div class="display-big-show">
+      <div class="header d-flex absolute align-center mx-7">
+        <div class="header__logo">
+          <img src="~/assets/logo.png" />
+        </div>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          name: 'Links',
+          route: '/',
+        },
+        {
+          id: 2,
+          name: 'Domains',
+          route: '/a',
+        },
+        {
+          id: 3,
+          name: 'Workspaces',
+          route: '/b',
+        },
+        {
+          id: 4,
+          name: 'Contact',
+          route: '/c',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  font-family: Montserrat, sans-serif;
+  ul {
+    list-style: none;
+    display: flex;
+    li {
+      margin-right: 32px;
+    }
+    li:last-child {
+      margin-right: 0;
+    }
+  }
+  &__logo {
+    img {
+      object-fit: cover;
+      width: 50px;
+      height: auto;
+    }
+  }
+  &__login {
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 18px;
+    color: #3c64b1;
+  }
+  &__signup {
+    background: #3c64b1;
+    border-radius: 10px;
+    .signup-text {
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 18px;
+      color: #fff;
+    }
+    &:hover {
+      background-color: #2a5bd7;
+    }
+  }
+  &__menu {
+    .menu-text {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 18px;
+      color: #000;
+    }
+  }
+
+  .nuxt-link-active {
+    .header__menu {
+      .menu-text {
+        color: #3c64b1;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 18px;
+      }
+    }
+  }
+}
+
+.display-small-hidden {
+  display: none;
+  @media (min-width: 660px) {
+    display: block;
+  }
+}
+
+.display-big-show {
+  display: block;
+  @media (min-width: 660px) {
+    display: none;
+  }
+}
+</style>
---
+++ layouts/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <div id="header" class="relative">
+      <Header class="header-fixed" />
+    </div>
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+import Header from '@/components/shares/Header';
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#header {
+  position: relative;
+  z-index: 999;
+}
+.header-fixed {
+  position: fixed;
+  background-color: #fff;
+  width: 100%;
+}
+</style>
