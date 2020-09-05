cdaeb8d5f291f49e6372b05e07163f5e64128b57
Do Anh Tu <anhtudo97@gmail.com>
2020-09-05 11:08:28 +0700

    feat: update UI sign in

--- components/login/Login.vue
+++ components/login/Login.vue
@@ -1,39 +1,39 @@
 <template>
   <div>
     <v-row id="login" no-gutters class="login overflow-hidden">
-      <v-col class="login__image" cols="12" sm="7" lg="8">
-        <div class="image"></div>
-      </v-col>
       <v-col
-        class="pa-xs-10 pa-sm-7 pa-5 d-flex flex-column align-self-center login__form-login"
-        cols="12"
-        sm="5"
-        lg="4"
+        cols="11"
+        md="10"
+        lg="7"
+        class="login__block mx-auto d-flex flex-column justify-center"
       >
-        <div class="login-image text-center">
-          <img src="~/assets/logo.png" />
-        </div>
-        <div class="text-center login-title">Login to your dashboard</div>
-        <v-sheet>
-          <form action>
-            <v-text-field class="login-email" label="Your Email" outlined></v-text-field>
-            <v-text-field class="login-password" type="password" label="Your Password" outlined></v-text-field>
-            <button class="login-button" @click="routeToLinks">
-              <div class="button-text my-3">Login</div>
-            </button>
-          </form>
-        </v-sheet>
-        <div class="an-account text-center mt-3">
-          Don't have an account ?
-          <nuxt-link to="/sign-up">
-            <strong class="account-signup">Sign up</strong>
-          </nuxt-link>
-        </div>
-        <nuxt-link to="/">
-          <div
-            class="forgot-password text-center account-signup account-signup mt-1"
-          >Forgot password</div>
-        </nuxt-link>
+        <v-row>
+          <v-col cols="12" sm="6" class="text-center text-sm-left login__left">
+            <figure>
+              <img src="@/assets/signin-image.jpg" alt="sing up image" />
+            </figure>
+            <div class="create-account text-center">
+              <a href="#">Create an account</a>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="6" class="login__right">
+            <div class="login-title">Sign in</div>
+            <v-text-field class="input-name mt-4 mt-sm-6" label="Your name" hide-details="auto"></v-text-field>
+            <v-text-field
+              class="input-password mt-4 mt-sm-6"
+              label="Password"
+              type="password"
+              hide-details="auto"
+            ></v-text-field>
+            <v-checkbox
+              class="input-checkbox mt-6 mt-sm-8"
+              label="Remember me"
+              color="secondary"
+              value="secondary"
+            ></v-checkbox>
+            <button class="button-normal login-button mt-1 mt-sm-3" @click="routeToLinks">Login</button>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </div>
@@ -51,129 +51,115 @@ export default {
 
 <style lang="scss" scoped>
 .login {
-  font-family: Montserrat, sans-serif;
-  &__image {
-    background-image: url('https://freelancer-free.johnleider.com/img/aboutme.f240a572.png');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    z-index: 1;
-    min-height: 100vh;
+  font-family: Poppins, sans-serif;
+  &__block {
+    margin: 150px 0;
+    padding: 50px 100px;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.4) 0 0 10px;
   }
-  &__form-login {
-    .login-image {
-      img {
-        object-fit: cover;
-        width: 150px;
-        height: auto;
-      }
+  &__left {
+    .create-account {
+      width: 100%;
     }
+  }
+  &__right {
     .login-title {
-      font-size: 24px;
-      line-height: 36px;
-      margin-bottom: 35px;
+      line-height: 1.66;
+      margin: 0;
+      padding: 0;
+      font-weight: bold;
+      color: #222;
+      font-size: 36px;
     }
     .login-button {
-      width: 100%;
-      height: 50px !important;
-      background: #3c64b1 !important;
-      border-radius: 10px;
-      .button-text {
-        font-weight: 600;
-        color: white;
-        font-size: 16px;
-      }
-    }
-    .account-signup {
-      font-weight: 600;
-      color: #3c64b1;
+      padding: 7px 7vh;
+      font-weight: 500;
     }
   }
-  @media (max-width: 1368px) {
-    &__form-login {
-      .login-image {
-        img {
-          width: 120px;
-        }
+  @media screen and (max-width: 1368px) {
+    &__block {
+      padding: 50px 70px;
+    }
+    &__left {
+      .create-account {
+        font-size: 15px;
       }
+    }
+    .input-name::v-deep label,
+    .input-name::v-deep input,
+    .input-password::v-deep label,
+    .input-password::v-deep input,
+    .input-checkbox::v-deep label,
+    .input-checkbox::v-deep input {
+      font-size: 15px;
+    }
+    &__right {
       .login-title {
-        font-size: 22px;
-        line-height: 34px;
-        margin-bottom: 30px;
+        font-size: 32px;
       }
       .login-button {
-        .button-text {
-          font-size: 15px;
-        }
-      }
-      .an-account,
-      .forgot-password {
         font-size: 15px;
-      }
-      .login-email::v-deep label {
-        font-size: 15px;
-      }
-      .login-email::v-deep input {
-        font-size: 15px;
-      }
-      .login-password::v-deep label {
-        font-size: 15px;
-      }
-      .login-password::v-deep input {
-        font-size: 15px;
+        padding: 5px 6.5vh;
+        font-weight: 500;
       }
     }
   }
-  @media (max-width: 960px) {
-    &__form-login {
-      .login-image {
-        img {
-          width: 110px;
-        }
+  @media screen and (max-width: 960px) {
+    &__block {
+      margin: 100px 0;
+      padding: 40px 60px;
+    }
+    &__left {
+      .create-account {
+        font-size: 14px;
       }
+    }
+    .input-name::v-deep label,
+    .input-name::v-deep input,
+    .input-password::v-deep label,
+    .input-password::v-deep input,
+    .input-checkbox::v-deep label,
+    .input-checkbox::v-deep input {
+      font-size: 14px;
+    }
+    &__right {
       .login-title {
-        font-size: 20px;
-        line-height: 32px;
-        margin-bottom: 25px;
+        font-size: 30px;
       }
       .login-button {
-        width: 100%;
-        .button-text {
-          font-size: 14px;
-        }
-      }
-      .an-account,
-      .forgot-password {
         font-size: 14px;
-      }
-      .login-email::v-deep label {
-        font-size: 14px;
-      }
-      .login-email::v-deep input {
-        font-size: 14px;
-      }
-      .login-password::v-deep label {
-        font-size: 14px;
-      }
-      .login-password::v-deep input {
-        font-size: 14px;
+        padding: 5px 6vh;
+        font-weight: 500;
       }
     }
   }
-  @media (max-width: 600px) {
-    &__image {
-      min-height: 40vh;
+  @media screen and (max-width: 600px) {
+    &__block {
+      margin: 100px 0;
+      padding: 30px 50px;
     }
-    &__form-login {
-      .login-image {
-        img {
-          width: 110px;
-        }
-      }
-      .an-account,
-      .forgot-password {
+    &__left {
+      .create-account {
         font-size: 13px;
+      }
+    }
+    .input-name::v-deep label,
+    .input-name::v-deep input,
+    .input-password::v-deep label,
+    .input-password::v-deep input,
+    .input-checkbox::v-deep label,
+    .input-checkbox::v-deep input {
+      font-size: 13px;
+    }
+    &__right {
+      .login-title {
+        font-size: 28px;
+      }
+      .login-button {
+        font-size: 13px;
+        padding: 5px 5.5vh;
+        font-weight: 500;
       }
     }
   }
